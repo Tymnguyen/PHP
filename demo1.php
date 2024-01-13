cdfe4a2fb0e059a46e0e3961436f1ac546335dd6
Tym <nguyenmixx2001@gmail.com>
2024-01-13 13:51:58 +0700

    chuc nang a

---
+++ demo1.php
@@ -0,0 +1,16 @@
+<?php
+    echo "hello world!";
+?>
+<?php 
+echo "<br/>";
+$frist = 'hello';
+echo $frist;
+echo "<br/>";
+$frist = 'hello world!';
+echo $frist;
+?>
+<?php
+echo "<br/>";
+define('NAME', 'Nguyen Thi Diem Huong');
+echo NAME;
+?>
